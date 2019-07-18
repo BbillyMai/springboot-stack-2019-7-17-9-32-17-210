53332610d39dfbc6856729b886d0ede1f8c3e507
bbillymai <1363543881@qq.com>
2019-07-18 15:52:23 +0800

    add application-local.yml

--- src/main/java/com/tw/apistackbase/ApiStackBaseApplication.java
+++ src/main/java/com/tw/apistackbase/ApiStackBaseApplication.java
@@ -2,8 +2,10 @@ package com.tw.apistackbase;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource(value = "classpath:application-local.yml")
 public class ApiStackBaseApplication {
 
 	public static void main(String[] args) {
